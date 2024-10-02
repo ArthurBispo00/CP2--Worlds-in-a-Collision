449bf865cee67f038fd1e14ca595f7606d714903
joao1015 <moreira.joaopaulo1993@gmail.com>
2024-10-02 20:55:04 -0300

    Resolvendo Conflitos Da pagina Menu

--- my-app/src/Components/Menu/index.tsx
+++ my-app/src/Components/Menu/index.tsx
@@ -2,35 +2,29 @@ import Link from "next/link";
 
 export default function Menu() {
   return (
-<<<<<<< HEAD
-    <nav className="bg-gray-1000 p-4">
+    <nav className="bg-gray-800 p-4">
       <div className="max-w-7xl mx-auto">
-        <ul className="flex justify-around space-x-2">
+        <ul className="flex justify-around space-x-4">
           <li>
-            <Link href="/" className="text-white hover:text-blue-400 text-lg">
-              Home
+            <Link href="/" legacyBehavior>
+              <a className="text-white hover:text-blue-400 text-lg">Home</a>
             </Link>
           </li>
           <li>
-            <Link href="/Inicio" className="text-white hover:text-blue-400 text-lg">
-              Worlds
+            <Link href="/Inicio" legacyBehavior>
+              <a className="text-white hover:text-blue-400 text-lg">Worlds</a>
             </Link>
           </li>
           <li>
-            <Link href="/Artigos" className="text-white hover:text-blue-400 text-lg">
-              Artigos
+            <Link href="/Artigos" legacyBehavior>
+              <a className="text-white hover:text-blue-400 text-lg">Artigos</a>
             </Link>
           </li>
-=======
-    <nav className="menu">
-        <ul>
-            <li> <Link href="/">Home</Link> </li>
-           <li> <Link href="Artigos">Artigos</Link> </li>
-           <li> <Link href="Catastrofes">As cátastrofes de Velikovsky</Link> </li>
-
-            
-            
->>>>>>> ArthurBispo
+          <li>
+            <Link href="/Catastrofes" legacyBehavior>
+              <a className="text-white hover:text-blue-400 text-lg">As Catástrofes de Velikovsky</a>
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
